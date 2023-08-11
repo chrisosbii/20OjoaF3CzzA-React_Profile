1dd86805d851fae0fdecb8ca3d1321bb1f81798d
Chris Osborne <chrisosbii@gmail.com>
2023-08-11 12:41:53 -0500

    updated about page

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,30 +1,36 @@
 import profilePic from '../images/chris_osborne.jpg';
 import React from 'react';
+import { Avatar, Container, Box } from '@mui/material'
 
 console.log(profilePic);
 
 function About() {
     return (
-        <div>
-            <h1>About Me</h1>
+        <Container maxWidth="md">
+            
+                <h1>About Me</h1>
 
-            <div id="profilePic">
-                <img src={profilePic} alt="Chris Osborne's Picture" height="100%" width="100%">
-                </img>
-            </div>
-
-            <div className="contentBox">
-                <p>
-                    My name is Chris Osborne and I like coding in my free time. I have created a few projects on my own.
-                </p>
-                <p>
-                    Other then coding I enjoy road and mountain biking, jujitsu, playing with pets.  
-                </p>
-                <p>
-                    I currently live in Wisconsin and have discovered that cheese is great. Personally I enjoy a good sharp cheader 
-                </p>
-           </div>
-        </div>
+                <Avatar
+                    alt="Chris Osborne"
+                    src={profilePic}
+                    sx={{ width: 200, height: 200 }}
+                />
+            
+            <Box >
+                <div className="contentBox">
+                    <p>
+                        My name is Chris Osborne and I like coding in my free time. I have created a few projects on my own.
+                    </p>
+                    <p>
+                        Other then coding I enjoy road and mountain biking, jujitsu, playing with pets.  
+                    </p>
+                    <p>
+                        I currently live in Wisconsin and have discovered that cheese is great. Personally I enjoy a good sharp cheader 
+                    </p>
+                </div>
+            </Box >
+        </Container>
+        
     );
 }
 
